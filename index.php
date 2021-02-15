1af998cb3f6ff34f921f8c795ad79fa55fd88328
Vaidmine <73877238+Vaidmine@users.noreply.github.com>
2021-02-15 16:13:23 +0200

    salygos

--- index.php
+++ index.php
@@ -1,4 +1,112 @@
-<h4 style="color:rgb(0, 200, 255); padding-left:80px;">***** ND 1 ****** </h4>
+<h3 style="color:rgb(0, 200, 255); padding-left:80px;">***** ND 1 ****** </h3>
+// 1.	Sugeneruokite masyvą iš 30 elementų 
+// (indeksai nuo 0 iki 29), 
+// kurių reikšmės yra atsitiktiniai skaičiai nuo 5 iki 25.
+<?php
+
+
+  
+?>
+
+<h3 style="color:rgb(0, 200, 255); padding-left:80px;">***** ND 2 ****** </h3>
+// 2.	Naudodamiesi 1 uždavinio masyvu:
+// a)	Suskaičiuokite kiek masyve yra reikšmių didesnių už 10;
+// b)	Raskite didžiausią masyvo reikšmę;
+// c)	Suskaičiuokite visų reikšmių sumą;
+// d)	Sukurkite naują masyvą, kurio reikšmės yra 1 uždavinio masyvo reikšmes minus tos reikšmės indeksas;
+// e)	Papildykite masyvą papildomais 10 elementų su reikšmėmis nuo 5 iki 25, kad bendras masyvas padidėtų iki indekso 39;
+// f)	Iš masyvo elementų sukurkite du naujus masyvus. Vienas turi būti sudarytas iš neporinių indekso reikšmių, o kitas iš porinių;
+// g)	Masyvo elementus su poriniais indeksais padarykite lygius 0 jeigu jie didesni už 15;
+// h)	Suraskite pirmą (mažiausią) indeksą, kurio elemento reikšmė didesnė už 10;
+// i)	Naudodami funkciją unset() iš masyvo ištrinkite visus elementus turinčius porinį indeksą;
+
+<?php
+
+
+  
+?>
+
+<h3 style="color:rgb(0, 200, 255); padding-left:80px;">***** ND 3 ****** </h3>
+// 3.	Sugeneruokite masyvą, kurio reikšmės atsitiktinės 
+// raidės A, B, C ir D, o ilgis 200. 
+// Suskaičiuokite kiek yra kiekvienos raidės.
+
+<?php
+
+
+  
+?>
+
+<h3 style="color:rgb(0, 200, 255); padding-left:80px;">***** ND 4 ****** </h3>
+// 4.	Išrūšiuokite 3 uždavinio masyvą pagal abecėlę.
+
+<?php
+
+
+  
+?>
+
+
+<h3 style="color:rgb(0, 200, 255); padding-left:80px;">***** ND 5 ****** </h3>
+// 5.	Sugeneruokite 3 masyvus pagal 3 uždavinio sąlygą. 
+// Sudėkite masyvus, sudėdami atitinkamas reikšmes.
+// Paskaičiuokite kiek unikalių reikšmių kombinacijų gavote.
+
+<?php
+
+
+  
+?>
+
+<h3 style="color:rgb(0, 200, 255); padding-left:80px;">***** ND 6 ****** </h3>
+// 6.	Sugeneruokite du masyvus, kurių reikšmės yra 
+// atsitiktiniai skaičiai nuo 100 iki 999. 
+// Masyvų ilgiai 100. 
+// Masyvų reikšmės turi būti unikalios savo masyve (t.y. neturi kartotis).
+
+<?php
+
+
+  
+?>
+
+<h3 style="color:rgb(0, 200, 255); padding-left:80px;">***** ND 7 ****** </h3>
+// 7.	Sugeneruokite masyvą, kuris būtų sudarytas iš reikšmių, 
+// kurios yra pirmame 6 uždavinio masyve, 
+// bet nėra antrame 6 uždavinio masyve.
+
+<?php
+
+
+  
+?>
+
+<h3 style="color:rgb(0, 200, 255); padding-left:80px;">***** ND 8 ****** </h3>
+// 8.	Sugeneruokite masyvą iš elementų, 
+// kurie kartojasi abiejuose 6 uždavinio masyvuose.
+
+<?php
+
+
+  
+?>
+
+<h3 style="color:rgb(0, 200, 255); padding-left:80px;">***** ND 9 ****** </h3>
+// 9.	Sugeneruokite masyvą, kurio indeksus sudarytų 
+// pirmo 6 uždavinio masyvo reikšmės, 
+// o jo reikšmės iš būtų antrojo masyvo.
+<?php
+
+
+  
+?>
+
+<h3 style="color:rgb(0, 200, 255); padding-left:80px;">***** ND 10 ****** </h3>
+// 10.	Sugeneruokite 10 skaičių masyvą pagal taisyklę: 
+// Du pirmi skaičiai- atsitiktiniai nuo 5 iki 25.
+// Trečias, pirmo ir antro suma. 
+// Ketvirtas- antro ir trečio suma. 
+// Penktas trečio ir ketvirto suma ir t.t.
 <?php
 
 
