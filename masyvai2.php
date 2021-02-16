0eddc3b65678427dbd4ffdc029ee1720fc07e172
Vaidmine <73877238+Vaidmine@users.noreply.github.com>
2021-02-16 21:43:43 +0200

    masuvai

--- masyvai2.php
+++ masyvai2.php
@@ -85,54 +85,136 @@ echo "Unikalių reikšmių kombinacijų viso yra:";
 // Masyvų reikšmės turi būti unikalios savo masyve (t.y. neturi kartotis).
 echo "Sugeneruokite du masyvus, kurių reikšmės yra atsitiktiniai skaičiai nuo 100 iki 999. <br>
 Masyvų ilgiai 100. Masyvų reikšmės turi būti unikalios savo masyve (t.y. neturi kartotis).<br>";
-$arrayOne = [];
-$randomOne[rand(100, 999)]);
+echo '<br>';
+$masyvasPirmas = [];
+$masyvasAntras = [];
 for ($i=0; $i < 100 ; $i++) { 
-    # code...
+    $randomReiksmes01 = rand(100, 999);
+    array_push($masyvasPirmas, $randomReiksmes01);
+    $randomReiksmes02 = rand(100, 999);
+    array_push($masyvasAntras, $randomReiksmes02);
 }
-  
-$arrayTwo = [];
+ //*****************************/
+
+$masyvasTrecias = [];
+foreach($masyvasPirmas as $value){
+    if(!in_array($value, $masyvasTrecias)){
+        array_push($masyvasTrecias, $value);
+    }
+}
+echo " <b> Vienas naujas masyvas su unikaliomis reikšmėmis </b> ";
+echo '<pre>';
+print_r ($masyvasTrecias);
+echo '</pre><br>';
+
+ //*****************************
+ // !in_array — Checks if a value exists in an array
+ // array_push — Push one or more elements onto the end of array
+
+$masyvasKetvirtas = [];
+foreach($masyvasAntras as $value){
+    if(!in_array($value, $masyvasKetvirtas)){
+        array_push($masyvasKetvirtas, $value);
+    }
+}
+echo " <b> Kitas naujas masyvas su unikaliomis reikšmėmis </b> ";
+echo '<pre>';
+print_r ($masyvasKetvirtas);
+echo '</pre><br>';
+ 
 ?>
 
 <h3 style="color:rgb(0, 200, 255); padding-left:80px;">***** ND 7 ****** </h3>
-// 7.	Sugeneruokite masyvą, kuris būtų sudarytas iš reikšmių, 
+<?php
+ // 7.	Sugeneruokite masyvą, kuris būtų sudarytas iš reikšmių, 
 // kurios yra pirmame 6 uždavinio masyve, 
 // bet nėra antrame 6 uždavinio masyve.
+echo " Sugeneruokite masyvą, kuris būtų sudarytas iš reikšmių, <br>
+kurios yra pirmame 6 uždavinio masyve, bet nėra antrame 6 uždavinio masyve.<br>";
 
-<?php
+///count — Count all elements in an array, or something in an object
+$arrayKazkoksDar = [];
+for ($i = 0; $i < count($masyvasTrecias); $i++) {
+    if(!in_array($masyvasTrecias[$i], $masyvasKetvirtas)) {
+        array_push($arrayKazkoksDar, $masyvasTrecias[$i]);
+    }
+}
+echo " <b> Kažkoks dar vienas naujas masyvas su unikaliomis reikšmėmis </b> ";
+echo '<pre>';
+print_r($arrayKazkoksDar);
+echo '</pre><br>';
 
 
-  
 ?>
 
+
 <h3 style="color:rgb(0, 200, 255); padding-left:80px;">***** ND 8 ****** </h3>
-// 8.	Sugeneruokite masyvą iš elementų, 
-// kurie kartojasi abiejuose 6 uždavinio masyvuose.
 
 <?php
+// 8.	Sugeneruokite masyvą iš elementų, 
+// kurie kartojasi abiejuose 6 uždavinio masyvuose.
+echo " Sugeneruokite masyvą,iš elementų, <br>
+kurie kartojasi abiejuose 6 uždavinio masyvuose.<br>";
 
+$kartojasi = [];
+for ($i = 0; $i < count($masyvasTrecias); $i++) {
+    if (in_array($masyvasTrecias[$i], $masyvasKetvirtas)) {
+        array_push($kartojasi, $masyvasTrecias[$i]);
+    }
+}
+echo '<pre>';
+print_r($kartojasi);
+echo '</pre>';
+echo '<br><br>';
 
   
 ?>
 
 <h3 style="color:rgb(0, 200, 255); padding-left:80px;">***** ND 9 ****** </h3>
+
+<?php
 // 9.	Sugeneruokite masyvą, kurio indeksus sudarytų 
 // pirmo 6 uždavinio masyvo reikšmės, 
 // o jo reikšmės iš būtų antrojo masyvo.
-<?php
+echo "Sugeneruokite masyvą, kurio indeksus sudarytų pirmo 6 uždavinio masyvo reikšmės, <br>
+o jo reikšmės iš būtų antrojo masyvo.<br>";
+$jauDarosiVisiskaNesamone = [];
+for($i = 0; $i < count($masyvasTrecias); $i++) {
+    $jauDarosiVisiskaNesamone[$masyvasTrecias[$i]] = $masyvasKetvirtas[$i];
+
+}
+echo "as jau nebesuprantu....";
+echo '<pre>';
+print_r($jauDarosiVisiskaNesamone);
+echo '</pre>';
 
 
-  
+ 
 ?>
+  
 
 <h3 style="color:rgb(0, 200, 255); padding-left:80px;">***** ND 10 ****** </h3>
+
+<?php
 // 10.	Sugeneruokite 10 skaičių masyvą pagal taisyklę: 
 // Du pirmi skaičiai- atsitiktiniai nuo 5 iki 25.
-// Trečias, pirmo ir antro suma. 
+// Trečias yra pirmo ir antro suma. 
 // Ketvirtas- antro ir trečio suma. 
 // Penktas trečio ir ketvirto suma ir t.t.
-<?php
 
+$skaiciuMasyvas = [];
+for ($i=0; $i < 10 ; $i++) { 
+    if ($i < 2) {
+        $skaiciuMasyvas[$i] = rand(5, 25);
+    } else {
+        $skaiciuMasyvas[$i] = ($skaiciuMasyvas[$i - 2] + $skaiciuMasyvas[$i - 1]);
+    }
+}
 
+echo "10 skaičių masyvas, kai du pirmi skaičiai- atsitiktiniai nuo 5 iki 25. <br>
+Trečias yra pirmo ir antro suma. Ketvirtas- antro ir trečio suma ir t.t. ";
+echo '<pre>';
+print_r($skaiciuMasyvas);
+echo '</pre>';
   
 ?>
